e4e206229ccea8dcba6b5ce021bfc18c27212287
amoskarugo <karugoamos@gmail.com>
2024-05-16 13:21:49 +0300

    basic cache policies implementation

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+module containing a class for performing
+cache operations.
+"""
+
+from BaseCaching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    a class that implements some cache operations
+    """
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get item by key"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data[key]
