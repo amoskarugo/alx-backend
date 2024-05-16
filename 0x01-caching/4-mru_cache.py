609eba523d0db4fa2eeeac76bd299823ee79944e
amoskarugo <karugoamos@gmail.com>
2024-05-16 20:44:17 +0300

    #

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+"""module containing a class
+to implement MRU caching"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """implement MRU caching operations"""
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+            self.cache_data[key] = item
+        else:
+            item_key = list(self.cache_data.items())[0][0]
+            self.cache_data.pop(item_key)
+            print(f'DISCARD: {item_key}')
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get item by key"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data[key]
